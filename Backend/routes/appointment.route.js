22a23ecd67d81d5def6cb5cf63e4864265371c92
Kiran Kasana <kkasanacoder@gmail.com>
2025-12-07 20:51:56 +0530

    add star rating in feedback form

--- Backend/routes/appointment.route.js
+++ Backend/routes/appointment.route.js
@@ -129,7 +129,7 @@ AppointmentRoutes.post("/Book_Appointment", authMiddleware(["doctor", "patient"]
 
 
 
-AppointmentRoutes.get("/showAppointment", authMiddleware(["doctor" , "patient"]), async (req, res) => {
+AppointmentRoutes.get("/showAppointment", authMiddleware(["doctor"]), async (req, res) => {
   try {
     const appointments = await AppointmentModel.find().populate("userId", "username email role").sort({ BookDate: 1 });
 
@@ -164,7 +164,7 @@ AppointmentRoutes.post("/sendNotification", async (req, res) => {
 
 
 
-AppointmentRoutes.get("/analytics/overview", authMiddleware(["doctor","admin","patient"]), async (req, res) => {
+AppointmentRoutes.get("/analytics/overview", authMiddleware(["doctor","admin"]), async (req, res) => {
     try {
       const totalAppointments = await AppointmentModel.countDocuments();
       const completed = await AppointmentModel.countDocuments({ status: "completed" });
@@ -183,7 +183,7 @@ AppointmentRoutes.get("/analytics/overview", authMiddleware(["doctor","admin","p
 
 
 
-AppointmentRoutes.get("/analytics/trends", authMiddleware(["doctor", "admin","patient"]), async (req, res) => {
+AppointmentRoutes.get("/analytics/trends", authMiddleware(["doctor", "admin"]), async (req, res) => {
   try {
     const trends = await AppointmentModel.aggregate([ {
         $group: {
@@ -203,7 +203,7 @@ AppointmentRoutes.get("/analytics/trends", authMiddleware(["doctor", "admin","pa
 
 
 
-AppointmentRoutes.get("/analytics/provider-performance", authMiddleware(["doctor" , "admin","patient"]), async (req, res) => {
+AppointmentRoutes.get("/analytics/provider-performance", authMiddleware(["doctor" , "admin"]), async (req, res) => {
   try {
     const performance = await AppointmentModel.aggregate([
       {
@@ -231,7 +231,7 @@ AppointmentRoutes.get("/analytics/provider-performance", authMiddleware(["doctor
 
 
 
-AppointmentRoutes.get("/analytics/predict-cancellations", authMiddleware(["admin" , "doctor" ,"patient"]), async (req, res) => {
+AppointmentRoutes.get("/analytics/predict-cancellations", authMiddleware(["admin" , "doctor"]), async (req, res) => {
   try {
     const data = await AppointmentModel.find({}, "patientName status createdAt");
     const risk = data.map((a) => {
@@ -270,7 +270,7 @@ AppointmentRoutes.put( "/update-expired", authMiddleware(["admin", "doctor"]),  
 );
 
 
-AppointmentRoutes.get("/upcoming", authMiddleware(["doctor", "patient"]), async (req, res) => {
+AppointmentRoutes.get("/upcoming", authMiddleware(["doctor"]), async (req, res) => {
     try {
       const userId = req.user;
 
